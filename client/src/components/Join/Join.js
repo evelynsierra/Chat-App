461030b01a4441d3d70fc96ec73c9fbecbfaf829
Sierra <55088939+evelynsierra@users.noreply.github.com>
2021-11-29 12:01:16 +0700

    change heading

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -10,7 +10,7 @@ const Join = () => {
     return (
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
-                <h1 className="heading"> Join </h1>
+                <h1 className="heading"> Sign In </h1>
                 <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
                 <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
                 <Link oncClick={event => (!name || !room) ? event.preventDefault(): null} to={`/chat?name=${name}&room=${room}`}>
