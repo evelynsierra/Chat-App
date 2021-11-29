0121171a024087abdf07f2c1edb4fee7db85a937
Sierra <55088939+evelynsierra@users.noreply.github.com>
2021-11-29 16:02:33 +0700

    emit server

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
+
+
+import { useLocation } from 'react-router';
 import './Chat.css';
 
+let socket;
 const Chat = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    const location = useLocation();
+    useEffect(()=> {
+        const {name, room} = queryString.parse(location.search);
+        console.log(name,room);
+
+        socket=io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', {name, room}, ({error }) => {
+            alert(error);
+        });
+    },[ENDPOINT, location.search]);
+
     return (
         <h1>Chat</h1>
     )
--- server/index.js
+++ server/index.js
@@ -8,12 +8,24 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {cors: {
+  origin: '*',
+  }
+});
 
 io.on('connection', (socket) => {
     console.log('We have a new connection!');
+
+    socket.on('join', ({name,room}, callback) => {
+      console.log(name,room);
+
+      const error = true;
+      if(error) {
+        callback({error :'error'});
+      }
+    })
     socket.on('disconnect', () => {
-      console.log('User had left!');
+      console.log('User had left!'); 
     });
   });
 
