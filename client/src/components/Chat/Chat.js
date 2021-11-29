f9b565398d323929120d3f4ca071d52951ff1a70
Sierra <55088939+evelynsierra@users.noreply.github.com>
2021-11-29 16:43:50 +0700

    user connect and disconnected

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -23,9 +23,15 @@ const Chat = () => {
         setName(name);
         setRoom(room);
 
-        socket.emit('join', {name, room}, ({error }) => {
-            alert(error);
+        socket.emit('join', {name, room}, () => {
+
         });
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
     },[ENDPOINT, location.search]);
 
     return (
